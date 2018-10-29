71b998023e46d557b9d9c7c39459e6a1db19f823
Fkfkkrkfew0 <44093001+Fkfkkrkfew0@users.noreply.github.com>
2018-10-29 20:08:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -64,7 +64,7 @@ client.on('message', message => {
   let args = message.content.split(" ").slice(1);
 
 
-if(command == "say") {
+if(command == "lol") {
     var Canvas = require('canvas')
   , Image = new Canvas.Image
   , canvas = new Canvas(450, 170)
