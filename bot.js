6ca7fe871582924f54325073670e8fc0ec13e702
Fkfkkrkfew0 <44093001+Fkfkkrkfew0@users.noreply.github.com>
2018-10-29 19:58:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,20 +56,38 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('
 
 
 
+client.on('message', message => {
+    var prefix = "$"
+    let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
 
-cclient.on('message', message => {
-    if (message.content === '$spam') {
-          let count = 0;
-          let ecount = 0;
-          for(let x = 0; x < 90000; x++) {
-            message.channel.send(`سباام يولد يلعن اومم الفله ${x}`)
-              .then(m => {
-                count++;
-              })
-              
-            }
-          }
-    });
+  let args = message.content.split(" ").slice(1);
+
+
+if(command == "draw") {
+    var Canvas = require('canvas')
+  , Image = new Canvas.Image
+  , canvas = new Canvas(450, 170)
+  , ctx = canvas.getContext('2d');
+  ctx.font = '30px Impact';
+  let args = message.content.split(" ").slice(1);
+  
+Image.src = canvas.toBuffer();
+
+    console.log(Image);
+ctx.drawImage(Image, 0, 0, Image.width / 470, Image.height / 170);
+ctx.fillText(args.join("  "),110, 70);
+
+
+ctx.beginPath();
+ctx.lineTo(50, 102);
+ctx.stroke();
+
+message.channel.sendFile(canvas.toBuffer());
+}
+}).on('ready', () => {
+
+});
     
 
 
