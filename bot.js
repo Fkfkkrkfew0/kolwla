bbec68f42e63ea4db0754bf8bd6848decb8a5e0e
Fkfkkrkfew0 <44093001+Fkfkkrkfew0@users.noreply.github.com>
2018-10-29 19:32:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,4 +20,34 @@ console.log("Scrpit By ESMAEL");
 
 
 
+
+
+
+
+       var prefix = "$";
+    client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "spam") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`áíÓ áÏíß ÕáÇÍíÇÊ`**');
+
+
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
