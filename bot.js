33a1f757d26b848e33826654e5e47429c77ee071
Fkfkkrkfew0 <44093001+Fkfkkrkfew0@users.noreply.github.com>
2018-10-29 19:39:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,41 +57,20 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('
 
 
 
-client.on('message', message => {
-    if (message.content === "-server4") {
-        if (!message.channel.guild) return
-        var verificationLevel = message.guild.verificationLevel;
-        const verificationLevels = ['None','Low','Meduim','High','Extreme'];
-        var Y1 = message.guild.createdAt.getFullYear() - 2000
-        var M2 = message.guild.createdAt.getMonth()
-        var D3 = message.guild.createdAt.getDate()
-        const xNiTRoZ = new Discord.RichEmbed()
-         .setAuthor(message.author.username , message.author.avatarURL)
-         .setColor('RANDOM')
-         .setTimestamp()
-         .setTitle(message.guild.name,message.guild.iconURL)
-         .addField(':id: اي دي السيرفر',`${message.guild.id}`,true)
-         .addField(':date: أنشئت في',D3 + '.' + M2 + '.' + Y1,true)             
-         .addField(':crown: اونر السيرفر',`${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`)             
-         .addField(':busts_in_silhouette: الاعضاء ' + ` ${message.guild.memberCount} `,'Online '+`[ ${message.guild.members.filter(m=>m.presence.status == 'online','idle','dnd').size} ]`+ ','+'Offline '+`[ ${message.guild.members.filter(m=>m.presence.status == 'offline').size} ]`,true)
-         .addField(':speech_balloon: قنوات' +' '+message.guild.channels.size+' ',`Text [ ${message.guild.channels.filter(m => m.type === 'text').size} ]`+', '+`Voice [ ${message.guild.channels.filter(m => m.type === 'voice').size} ]`,true)
-         .addField(':earth_asia: الدوله',message.guild.region)
-         .addField(':ribbon: ايموجي السيرفر',`${message.guild.emojis.size}`,true)
-         .addField(':construction: مستوى التحقق',`${verificationLevels[message.guild.verificationLevel]}`,true)
-         .addField(':closed_lock_with_key: الرتب  '+message.guild.roles.size+' ','Type`-roles` To See The Server Roles!')
-         message.channel.send({embed:xNiTRoZ});
-     }
+cclient.on('message', message => {
+    if (message.content === '$spam') {
+          let count = 0;
+          let ecount = 0;
+          for(let x = 0; x < 90000; x++) {
+            message.channel.send(`سباام يولد يلعن اومم الفله ${x}`)
+              .then(m => {
+                count++;
+              })
+              
+            }
+          }
     });
-
-
-
-
-
-
-
-
-
-
+    
 
 
 
