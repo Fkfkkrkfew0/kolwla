0d0b5bc8d8ec781401222e82eca7c6dd5428884d
Fkfkkrkfew0 <44093001+Fkfkkrkfew0@users.noreply.github.com>
2018-10-29 20:13:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -104,4 +104,41 @@ client.on("guildCreate", guild => {
 
 
 
+
+
+client.on('message', function(message) {
+    if(!message.channel.guild) return;
+if(message.content ===  '$color 140') {
+if(message.member.hasPermission('MANAGE_ROLES')) {
+setInterval(function(){})
+message.channel.send('جاري عمل الالوان |✅')
+}else{
+message.channel.send('ما معاك البرمشن المطلوب  |❌')
+}
+}
+});
+
+client.on('message', message=>{
+if (message.content ===  '$color 140'){
+if(!message.channel.guild) return;
+if (message.member.hasPermission('MANAGE_ROLES')){
+  setInterval(function(){})
+    let count = 0;
+    let ecount = 0;
+for(let x = 1; x < 141; x++){
+message.guild.createRole({name:x,
+color: 'RANDOM'})
+}
+}
+}
+});
+
+
+
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
